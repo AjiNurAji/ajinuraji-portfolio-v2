7312d38d808332e4627dff06c364a7c036b47947
AjiNurAji <ajinuraji090306@gmail.com>
2024-12-26 16:21:17 +0700

    :pen: remove configurate database

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->middleware("maintenance");
-
-Route::get("/configurate-database", function () {
-    Artisan::call("migrate:fresh --seed");
-    return response()->json([
-        "success" => true,
-        "message" => "Database has been configured"
-    ]);
-});
