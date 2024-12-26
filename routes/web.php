f7eb2306e78bc4751fc7af02941c5c9173cbd6a4
AjiNurAji <ajinuraji090306@gmail.com>
2024-12-26 15:33:08 +0700

    :beers: create database configurate

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->middleware("maintenance");
+
+Route::post("/configurate-database", function () {
+    Artisan::call("migrate:fresh --seed");
+    return response()->json([
+        "success" => true,
+        "message" => "Database has been configured"
+    ]);
+});
