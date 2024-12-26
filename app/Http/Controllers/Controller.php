0bda8bff9a44c56db777f6835f4e3bcae660a844
AjiNurAji <ajinuraji090306@gmail.com>
2024-12-26 15:26:52 +0700

    :beers: create maintenance system

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Site;
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    public function isMaintenance(Request $request)
+    {
+        $validated = $request->validate([
+            "site_name" => "string",
+            "is_maintenance" => "boolean"
+        ]);
+
+        $site = Site::where("site_name", $validated["site_name"])->first();
+
+        $site->is_maintenance = $validated["is_maintenance"];
+        $site->save();
+        return response()->json($site);
+    }
 }
